9f651b9b01226126b52de1af58db1202ffa7649d
Nathan Philippon <hockeyfangohabsgo@gmail.com>
2022-09-10 16:09:45 -0400

    Added sign in with Google.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const { OAuth2Client } = require('google-auth-library')
 const hitters = require('./services/hitters')
 const pitchers = require('./services/pitchers')
 
-require('dotenv').config()
+dotenv.config()
+const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID)
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -17,6 +20,25 @@ mongoose.connect(uri, { useNewUrlParser: true })
 const connection = mongoose.connection
 connection.once('open', () => {
     console.log('MongoDB database connection established successfully')
+})
+
+const users = []
+
+function upsert(array, item) {
+    const i = array.findIndex((_item) => _item.email === item.email)
+    i > -1 ? array[i] = item : array.push(item)
+}
+
+app.post('/api/google-login', async (req, res) => {
+    const { token } = req.body
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.CLIENT_ID,
+    });
+    const { name, email, picture } = ticket.getPayload()
+    upsert(users, { name, email, picture })
+    res.status(201)
+    res.json({ name, email, picture })
 })
 
 const usersRouter = require('./routes/users')
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import GoogleLogin from 'react-google-login'
+import { useEffect, useState } from 'react';
+import jwt_decode from "jwt-decode";
+
+
+import Navbar from "./components/navbar.component"
+import PlayersList from "./components/players-list.component"
 
 function App() {
+  
+  const [ user, setUser ] = useState({});
+
+  function handleCallbackResponse(response) {
+    console.log(`Encoded JWT ID token: ${response.credential}`);
+    let userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut(e) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id="signInDiv"></div>
+      { Object.keys(user).length != 0 && 
+        <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+      }
+      { user &&
+        <div>
+          <img src={user.picture}></img>
+          <h3>{user.name}</h3>
+        </div>
+      }
     </div>
   );
 }
